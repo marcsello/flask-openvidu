3bd536325314d8dd20536fe7718f2f0e3eb4a07b
marcsello <punkosdmarcell@rocketmail.com>
2020-04-12 01:55:18 +0200

    Setup requirements

--- setup.py
+++ setup.py
@@ -10,11 +10,11 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = [ ]
+requirements = ['pyopenvidu>=0.1.2', 'flask']
 
-setup_requirements = [ ]
+setup_requirements = []
 
-test_requirements = [ ]
+test_requirements = []
 
 setup(
     author="Marcell Pünkösd",
@@ -25,9 +25,12 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
+        'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Framework :: Flask',
+        'Topic :: Software Development :: Libraries :: Python Modules'
     ],
     description="Adds OpenVidu support to your Flask application",
     install_requires=requirements,
@@ -42,9 +45,9 @@ setup(
     tests_require=test_requirements,
     url='https://github.com/marcsello/flask_openvidu',
     project_urls={
-        "Documentation": "https://pyopenvidu.readthedocs.io/",
-        "Code": "https://github.com/marcsello/pyopenvidu",
-        "Issue tracker": "https://github.com/marcsello/pyopenvidu/issues",
+        "Documentation": "https://flask-openvidu.readthedocs.io/",
+        "Code": "https://github.com/marcsello/flask-openvidu",
+        "Issue tracker": "https://github.com/marcsello/flask-openvidu",
     },
     version='0.1.0',
     zip_safe=False,
