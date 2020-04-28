ba16aa595a31bf6905882e292f99587e33b0f125
marcsello <punkosdmarcell@rocketmail.com>
2020-04-29 01:18:20 +0200

    Updated requirements

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['pyopenvidu>=0.1.2', 'flask']
+requirements = ['pyopenvidu>=0.1.3', 'flask']
 
 setup_requirements = []
 
